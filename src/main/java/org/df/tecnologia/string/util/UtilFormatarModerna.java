98ab563cf4ec777b37f4b2b1beaf62b209a5a15c
Mac_os <samuelmelo@MacBook-Pro-de-Mac-os.local>
2025-06-02 12:30:19 -0300

    Evitando if's.

--- src/main/java/org/df/tecnologia/string/util/UtilFormatarModerna.java
+++ src/main/java/org/df/tecnologia/string/util/UtilFormatarModerna.java
@@ -3,6 +3,7 @@ package org.df.tecnologia.string.util;
 import java.lang.reflect.Field;
 import java.util.Optional;
 import java.util.function.Function;
+import java.util.stream.Stream;
 
 import org.apache.commons.text.WordUtils;
 import org.df.tecnologia.string.anotacoes.Formatar;
@@ -23,7 +24,7 @@ public class UtilFormatarModerna {
         Formatar anotacao = field.getAnnotation(Formatar.class);
         try {
             Optional.ofNullable((String) field.get(obj))
-                    .map(comporFormatacoes(anotacao))   // aplica todas as regras de formatação
+                    .map(comporFormatacoes2(anotacao))   // aplica todas as regras de formatação
                     .ifPresent(valorFormatado -> {
                         try {
                             field.set(obj, valorFormatado);
@@ -51,6 +52,22 @@ public class UtilFormatarModerna {
             }
             return resultado;
         };
+    }
+
+    /**
+     * Evita o uso dos if.
+     * @param anotacao
+     * @return
+     */
+    private static Function<String, String> comporFormatacoes2(Formatar anotacao) {
+        // Lista de funções de formatação, criadas condicionalmente via Stream
+        return Stream.<Function<String, String>>of(
+                        anotacao.trim()       ? String::trim : null,
+                        anotacao.upperCase()  ? String::toUpperCase : null,
+                        anotacao.FirstUpper() ? WordUtils::capitalizeFully : null
+                )
+                .filter(f -> f != null) // remove os nulos (funções não habilitadas)
+                .reduce(Function.identity(), Function::andThen); // encadeia tudo
     }
 
 }
